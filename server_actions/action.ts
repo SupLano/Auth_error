c36eac12e7854d1e5c2ceeaaf6edf8623570f784
SupLano <lawrenceoragwa@gmail.com>
2024-11-14 14:38:38 +0100

    latest

---
+++ server_actions/action.ts
@@ -0,0 +1,9 @@
+"use server"
+
+import { signIn } from "next-auth/react";
+
+
+export async function OneTapSignIn(credential?: string) {
+    "use server"
+    signIn("google-one-tap", { credential });
+  }
